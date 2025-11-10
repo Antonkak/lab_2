449b880a29ba72167a227dc0ef760ae43433edbe
Anton <antonef98@gmail.com>
2025-11-10 12:58:38 +0300

    logging fix

--- src/logging/logger.py
+++ src/logging/logger.py
@@ -1,8 +1,9 @@
 import logging
 from functools import wraps
+import sys
 
 def setup_logging():
-    """Настройка системы логирования"""
+    """Log config"""
     logging.basicConfig(
         filename='src/logging/shell.log',
         level=logging.INFO,
@@ -10,19 +11,19 @@ def setup_logging():
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
-def command_logger(func):
-    """Декоратор для логирования команд"""
+def command_logger(func, ):
+    """Decorator for logging commands"""
     @wraps(func)
     def wrapper(*args, **kwargs):
         setup_logging()
-        command_name = func.__name__
         try:
+            full_command = " ".join(sys.argv[1:])
             result = func(*args, **kwargs)
-            logging.info(f"{command_name}")
+            logging.info(f"{full_command}")
             logging.info("SUCCESS")
             return result
         except Exception as e:
-            logging.error(f"{command_name}")
+            logging.error(f"{full_command}")
             logging.error(f"ERROR: {e}")
             raise
     return wrapper
